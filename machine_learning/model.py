c22516b4a3c1d1b0af1c6dce4edda5197ccddc96
18220101 <18220101@mahasiswa.itb.ac.id>
2022-12-09 16:33:58 +0700

    model machine learning

--- machine_learning/model.py
+++ machine_learning/model.py
@@ -1,8 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+from database.connection import client
 
+db = client.insurance.insurance.find({},
+    {'_id':0, "id":0, "name":0})
+
+insurance_data = pd.DataFrame(list(db))
+
+# mengubah string di kolom sex, smoker, region menjadi int
+insurance_data.replace({'sex':{'male':0,'female':1}}, inplace=True)
+
+insurance_data.replace({'smoker':{'yes':0,'no':1}}, inplace=True)
+
+insurance_data.replace({'region':{'southeast':0,'southwest':1,'northeast':2,'northwest':3}}, inplace=True)
+
+X = insurance_data.drop(columns='charges', axis=1)
+Y = insurance_data['charges']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+insurance_model = LinearRegression()
+
+insurance_model.fit(X_train, Y_train)
+
+# prediction on training data
+training_data_prediction =insurance_model.predict(X_train)
+r2_train = metrics.r2_score(Y_train, training_data_prediction)
+
+# prediction on test data
+test_data_prediction =insurance_model.predict(X_test)
+r2_test = metrics.r2_score(Y_test, test_data_prediction)
--- routes/insurance.py
+++ routes/insurance.py
@@ -3,10 +3,12 @@ from database.connection import client
 from fastapi import APIRouter, Body, HTTPException, status
 from models.insurance import insurance
 from schemas.insurance import insuranceEntity, insurancesEntity
+from machine_learning.model import insurance_model
 from bson import ObjectId
+import numpy as np
 
 insurance_router = APIRouter(
-    tags=["Insurance"]
+    tags=["Prediction"]
 )
 
 @insurance_router.get("/", response_model=List[insurance])
@@ -17,9 +19,13 @@ async def retrieve_all_insurance() -> List[insurance]:
 async def retrieve_id_insurace(id: int) -> insurance:
     return insuranceEntity(client.insurance.insurance.find_one({"id":id}))
 
-@insurance_router.post("/new")
+@insurance_router.get("/new")
 async def add_insurance(body: insurance = Body(...)) -> dict:
     client.insurance.insurance.insert_one(dict(body))
     return {
         "message": "Insurance data added"
     }
+
+@insurance_router.post("/insurance_prediction")
+async def predict_insurance(body: insurance = Body(...)) -> dict:
+    return insurancesEntity(client.insurance.insurance.find())
