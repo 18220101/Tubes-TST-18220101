6139154382ac3934c7f827f05fb9aa383edbda48
18220101 <18220101@mahasiswa.itb.ac.id>
2022-12-09 12:54:36 +0700

    benerin connect mongodb

--- database/settings.py
+++ database/settings.py
@@ -1,2 +1,2 @@
-mongodb_uri = 'mongodb+srv://18220101:18220101@cluster0.gt4v60z.mongodb.net/test'
+mongodb_uri = 'mongodb+srv://18220101:18220101@cluster0.gt4v60z.mongodb.net/?retryWrites=true&w=majority'
 port = 8000
